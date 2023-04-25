988cca696a9ba0711c2f94575996cb39d5925285
realtempstudent <afakepseudouser@gmail.com>
2023-04-24 17:51:19 -0700

    lab 4 has been updated.

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,12 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock Paper Scissors
+// One possible implementation of the game
+// Author: Lily Zhao and Edward Oh
+// Date: 4/20/2023
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Pseudocode for RPS (Rock Paper Scissors)
+// Ask the user for input confirming their selection
+// When a valid input is received, the computer randomly chooses one of the three options of RPS
+// The computer displays which outputs have been chosen by both participants and then declares the result of the game based on what inputs have been provided.
+  // If both inputs are the same, the game declares a tie.
+  // Else, the computer displays which person has won based on whose input beats the other’s input.
+// After completion of the above steps, the game asks the user if they wish to play once more.
