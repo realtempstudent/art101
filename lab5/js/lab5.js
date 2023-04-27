dff70eb7c78cee830d14d2bfaebd6dce0db2327c
realtempstudent <afakepseudouser@gmail.com>
2023-04-26 20:33:09 -0700

    Lab 5 updated.

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Edward Oh <eoh4@ucsc.edu>
+ * Created: 4/25/2023
+ *  License: Public Domain
+ */
+
+// Define Variables
+make = "Toyota";
+model = "Sienna";
+color = "White";
+year = 1997;
+
+// calculate
+age = 2023 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
